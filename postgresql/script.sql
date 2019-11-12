a35d7b684a38c9fea90d63ce451ecde42db233b3
Jonathan Ellis <jde27@students.calvin.edu>
2019-11-12 11:06:16 -0500

    Update sqlScript

--- postgresql/script.sql
+++ postgresql/script.sql
@@ -2,35 +2,37 @@
 -- and populates the database with sample data
 --
 -- @author Shell Squad
--- @version October 30, 2019
+-- @version November 12, 2019
 --
 DROP TABLE IF EXISTS turtle, sighting;
 DROP TYPE IF EXISTS turtle_sex;
 
 CREATE TYPE turtle_sex AS ENUM('male', 'female');
 CREATE TABLE turtle (
-	id integer PRIMARY KEY,
+	id serial PRIMARY KEY,
+	turtle_number integer,
 	mark varchar(10),
 	sex turtle_sex 
 );
 CREATE TABLE sighting (
-	id integer PRIMARY KEY,
-	turtle_id integer,
+	id serial PRIMARY KEY,
+	turtle_id integer references turtle(id),
 	time_seen timestamp,
+	turtle_location varchar(50), 
 	latitude float8,
 	longitude float8,
 	carapace_length integer,
 	notes text
 );
 
-INSERT INTO turtle VALUES (0, '3R', 'male');
-INSERT INTO turtle VALUES (1, '2L', 'female');
-INSERT INTO turtle VALUES (2, '1L', 'male');
+INSERT INTO turtle (turtle_number, mark, sex) VALUES (1, '3R', 'male');
+INSERT INTO turtle (turtle_number, mark, sex) VALUES (2, '2L', 'female');
+INSERT INTO turtle (turtle_number, mark, sex) VALUES (3, '1L', 'male');
 
-INSERT INTO sighting VALUES (0, 0, '2019-10-26T02:15:31Z', 42.9313086715985, -85.58243000000002, 29, 'A sample sighting for turtle 0');
-INSERT INTO sighting VALUES (1, 0, '2019-10-26T03:18:09Z', 42.9320937253728, -85.58218392028273, 30, 'A second sample sighting for turtle 0');
-INSERT INTO sighting VALUES (2, 1, '2019-10-26T05:42:26Z', 42.9360273262836, -85.57836352739273, 25, 'A sample sighting for turtle 1');
-INSERT INTO sighting VALUES (3, 2, '2019-10-26T07:36:53Z', 42.9342562436253, -85.58038363535263, 28, 'A sample sighting for turtle 2');
+INSERT INTO sighting (turtle_id, time_seen, turtle_location, latitude, longitude, carapace_length, notes) VALUES (1, '2019-10-26T02:15:31Z', 'Field A', 42.9313086715985, -85.58243000000002, 29, 'A sample sighting for turtle 0');
+INSERT INTO sighting (turtle_id, time_seen, turtle_location, latitude, longitude, carapace_length, notes) VALUES (1, '2019-10-26T03:18:09Z', 'Field B', 42.9320937253728, -85.58218392028273, 30, 'A second sample sighting for turtle 0');
+INSERT INTO sighting (turtle_id, time_seen, turtle_location, latitude, longitude, carapace_length, notes) VALUES (2, '2019-10-26T05:42:26Z', 'Field C', 42.9360273262836, -85.57836352739273, 25, 'A sample sighting for turtle 1');
+INSERT INTO sighting (turtle_id, time_seen, turtle_location, latitude, longitude, carapace_length, notes) VALUES (3, '2019-10-26T07:36:53Z', 'Field D', 42.9342562436253, -85.58038363535263, 28, 'A sample sighting for turtle 2');
 
 
 -- some example queries
