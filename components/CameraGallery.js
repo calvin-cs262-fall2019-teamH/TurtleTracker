f150e07f13bad02a1570ca407e52185857edc344
David-Dahms <55364390+David-Dahms@users.noreply.github.com>
2019-11-21 08:40:33 -0500

    fixed previous gallery counter issue

--- components/CameraGallery.js
+++ components/CameraGallery.js
@@ -1,58 +1,116 @@
 import React, { Component } from 'react';
-import { Button, Image, View } from 'react-native';
+import { Button, Image, StyleSheet, View, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export default class CameraGallery extends Component {
 
-  // creates the placeholder question mark photo
+  // creates the image array and question mark placeholder image
   state = {
-    image: 'https://previews.123rf.com/images/tackgalichstudio/tackgalichstudio1405/tackgalichstudio140500025/28036032-question-mark-symbol-on-gray-background.jpg'
+    image_array: ['https://previews.123rf.com/images/tackgalichstudio/tackgalichstudio1405/tackgalichstudio140500025/28036032-question-mark-symbol-on-gray-background.jpg'],
   };
 
-  // takes an image using the camera
+  // takes an image using the camera and appends it to the image_array
   takeImage= async () => {
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
     });
-    this.setState({ image: result.uri });
-  };
+  
+  // replaces the questionmark placeholder image and appends new image to image_array
+  if ((!result.cancelled) && (this.state.image_array[0] == 'https://previews.123rf.com/images/tackgalichstudio/tackgalichstudio1405/tackgalichstudio140500025/28036032-question-mark-symbol-on-gray-background.jpg')) {
+    this.state.image_array.splice(0, 1);  
+    this.setState(prevState => ({
+      image_array: [...prevState.image_array, result.uri]
+  }))}
+
+  // appends the new image to the 
+  else if (!result.cancelled) {
+    this.setState(prevState => ({
+      image_array: [...prevState.image_array, result.uri]
+  }))}
+};
 
 // retrieves an image from a gallery
-  pickImage= async () => {
+  pickImage= async () => { 
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
+      allowsEditing: true
      });
-    this.setState({ image: result.uri });
-  };
 
+     // replaces the questionmark placeholder image and appends new image to image_array
+     if ((!result.cancelled) && (this.state.image_array[0] == 'https://previews.123rf.com/images/tackgalichstudio/tackgalichstudio1405/tackgalichstudio140500025/28036032-question-mark-symbol-on-gray-background.jpg')) {
+      this.state.image_array.splice(0, 1);  
+        this.setState(prevState => ({
+          image_array: [...prevState.image_array, result.uri]
+      }))}
+
+      // appends the new image to the 
+      else if (!result.cancelled) {
+        this.setState(prevState => ({
+          image_array: [...prevState.image_array, result.uri]
+      }))}
+    };
+
+  
   render() {
-    let { image } = this.state;
+    let { image_array } = this.state;
 
-    // creates the buttons and shows the selected image
+    // creates the buttons and shows the selected images
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
-        {image &&
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <View>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentContainer}>
+          <Image source={{uri: image_array[0], width: 200, height: 200}} style={styles.imageStyle}/>
+          <Image source={{uri: image_array[1], width: 200, height: 200}} style={styles.imageStyle}/>
+          <Image source={{uri: image_array[2], width: 200, height: 200}} style={styles.imageStyle}/>
+          <Image source={{uri: image_array[3], width: 200, height: 200}} style={styles.imageStyle}/>
+          <Image source={{uri: image_array[4], width: 200, height: 200}} style={styles.imageStyle}/>
+          <Image source={{uri: image_array[5], width: 200, height: 200}} style={styles.imageStyle}/> 
+        </ScrollView>
 
+      <View style={styles.takePicButtons}>
         <Button 
          title="take a new picture"
          onPress={this.takeImage}
          color = '#00E600'
          />
-         
+        
         <Button
           title="select from camera roll"
           onPress={this.pickImage}
           color = '#00B300'
         />
-        <View style={{flex: 1, height: 8}}></View>
-        
+      </View>
       </View>
     );
   }
 }
 
+const styles= StyleSheet.create({
+
+  contentContainer: {
+    paddingLeft: 65,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    alignSelf: 'center',
+  }, 
+
+  imageStyle: {
+    flex: 1,
+    margin: 15,
+  },
+
+  takePicButtons: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingBottom: 5,
+  },
+});
+
 // reference/source: https://docs.expo.io/versions/latest/sdk/imagepicker/
+
+
+
+
+
 
