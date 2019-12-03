34f2bfe144b9e6c5f47b0a457bc4c8419d1a991e
Jonathan Ellis <jde27@students.calvin.edu>
2019-12-03 11:21:40 -0500

    Fix edit crash

--- Screens/Sightings/SightingEditScreen.js
+++ Screens/Sightings/SightingEditScreen.js
@@ -145,7 +145,7 @@ export default function SightingEditScreen({ navigation }) {
             />
             <CameraGallery />
             {isEdit
-                ? <Button title="Submit" onPress={() => { editSightingById(sighting.id), navigation.state.params.refresh(), navigation.goBack() }} />
+                ? <Button title="Submit" onPress={() => { editSightingById(turtle.id), navigation.state.params.refresh(), navigation.goBack() }} />
                 : <Button title="Submit" onPress={() => { getLocationAndCreateSighting(turtle.id), navigation.navigate("TurtleView", { turtleId: turtle.id }) }} />}
         </ScrollView>
     );
